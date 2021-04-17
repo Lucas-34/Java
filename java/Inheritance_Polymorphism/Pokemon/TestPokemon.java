3c2230e37920ff8d48e1032dc7e7532442676fd0
Lbdj <>
2021-04-17 16:58:02 +0000

    derniere version

--- java/Inheritance_Polymorphism/Pokemon/TestPokemon.java
+++ java/Inheritance_Polymorphism/Pokemon/TestPokemon.java
@@ -2,8 +2,8 @@ class Main
 {
   public static void main(String[] args)
   {
-    Pokemon pokemon = new Pokemon("Pikachu", 5);
+    Pokemon pokemon_sportif = new SportsPokemon("Pikachu", 5, 2, 1, 1);
 
-    System.out.println("Un pokémon vient d'être créé. Son nom est " + pokemon.getName() + " et son poids est " + pokemon.getWeight());
+    System.out.println("Un pokémon vient d'être créé. Ses caractéristiques sont : \n Nom : " + pokemon_sportif.getName() + "\n Poids : " + pokemon_sportif.getWeight() + "\n Nombre de jambes : " + pokemon_sportif.getNbLegs() + "\n Taille : " + pokemon_sportif.getSize() + "\n Fréquence cardiaque : " + pokemon_sportif.getHeartRate());
   }
 }
